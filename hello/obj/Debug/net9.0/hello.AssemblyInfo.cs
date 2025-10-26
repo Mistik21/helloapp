4d1feabb0d98cb333432fb95c90802fe649ca477
white <white@example.com>
2025-10-26 16:35:16 +0500

    k

--- hello/obj/Debug/net9.0/hello.AssemblyInfo.cs
+++ hello/obj/Debug/net9.0/hello.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hello")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24b58e801150b25c662f922c1107f5cadd5a23ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4ff13a0852a866740e3b04b9a60db5c839b42e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("hello")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hello")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
